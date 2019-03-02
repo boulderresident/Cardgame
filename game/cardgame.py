9c71adceb651f569eb7e050e74637f02117b6f3a
David Fraser <davidfraser@mail.com>
2019-03-02 09:59:56 -0700

    added suggested changes to cardgme.py via #coments

--- game/cardgame.py
+++ game/cardgame.py
@@ -1,6 +1,9 @@
 import random
 
-#Probably need some kind of random seed
+#Changes to try:
+#add a random seed
+#change the suit arrays to multidimensional arrays e.g suit = [[5, "suit"], [6, "suit"]]
+#This change needs to be made because you cant tell what the sum of "10c"+"6h" is currently
 
 Clubs = ["Ac", "Kc", "Qc", "Jc", "10c ", "9c", "8c", "7c", "6c", "5c", "4c", "3c", "2c"];
 Spades = ["As", "Ks", "Qs", "Js", "10s", "9s", "8s", "7s", "6s", "5s", "4s", "3s", "2s", "1s"];
@@ -18,4 +21,3 @@ def rungame():
 def draw():
     draw = input("Enter any key to draw");
     print("You drew", deck[random.randint(1,4)-1][random.randint(1,13)-1]);
-    
